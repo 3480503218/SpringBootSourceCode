1178f0b5502eb5ec71603901e3543e2177aa10df
哈喽哈喽 <2461664778@qq.com>
2017-12-17 14:23:26 +0800

    update

--- highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/advice/ExceptionHandlerAdvice.java
+++ highlight_springmvc4/src/main/java/com/wisely/highlight_springmvc4/advice/ExceptionHandlerAdvice.java
@@ -9,22 +9,22 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.ModelAndView;
 
-@ControllerAdvice //1
+@ControllerAdvice //1控制器建言，全局配置放在同一位置
 public class ExceptionHandlerAdvice { 
 
-	@ExceptionHandler(value = Exception.class)//2
+	@ExceptionHandler(value = Exception.class)//2定义全局处理，value属性过滤拦截条件
 	public ModelAndView exception(Exception exception, WebRequest request) {
 		ModelAndView modelAndView = new ModelAndView("error");// error页面
 		modelAndView.addObject("errorMessage", exception.getMessage());
 		return modelAndView;
 	}
 
-	@ModelAttribute //3
+	@ModelAttribute //3绑定键值对放入Model中，全局的@RequestMapping都可以获得此处设置的键值对
 	public void addAttributes(Model model) {
 		model.addAttribute("msg", "额外信息"); //3
 	}
 
-	@InitBinder //4
+	@InitBinder //4设置WebDataBinder,用来自动绑定前台请求参数到Model中
 	public void initBinder(WebDataBinder webDataBinder) {
 		webDataBinder.setDisallowedFields("id"); //5
 	}
